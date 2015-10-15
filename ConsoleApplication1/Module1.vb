7c6945e1b41559745d10fb115441e5b8b5531b1f
The Lin <a245714555@gmail.com>
2015-10-15 21:06:06 +0800

    text

---
+++ ConsoleApplication1/Module1.vb
@@ -0,0 +1,19 @@
+﻿Module Module1
+
+    Dim a As Object
+    Dim b As Object
+
+    Private Property max As Object
+
+
+    Sub Main()
+
+        If a >= b Then
+            max = a
+        Else
+            max = a
+        End If
+    End Sub
+
+
+End Module
